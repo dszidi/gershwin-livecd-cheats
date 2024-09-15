22a576ef5f39ac6868202e3533230aa5f3c49266
Damian Szidiropulosz <damianos@knoxbug.org>
2024-09-15 08:06:42 -0400

    Add help text at end of prompt for common tasks. Added commented out git clone commands for common repos for convenience

--- debian.sh
+++ debian.sh
@@ -16,5 +16,15 @@ zshrc
 sudo apt update
 sudo apt install openssh-server neovim
 sudo systemctl start ssh
-xrandr --output Virtual-1 --mode 1920x1080 # "Output value may need changing depending on hypervisor or if we switch to Wayland"
 
+# Uncomment to fetch repos
+# cd && git clone https://github.com/gershwin-os/dock.git
+# cd && git clone https://github.com/gershwin-os/workspace.git
+# cd && git clone https://github.com/gershwin-os/system.git
+# cd && git clone https://github.com/gershwin-os/applications.git
+
+echo "Setup script completed"
+echo "Run 'source ~/.zshrc' to enable shell aliases"
+echo "###########"
+echo "Add the following line to /System/Library/Scripts/Gershwin-X11 before the Workspace is launched..."
+echo "xrandr --output Virtual-1 --mode 1920x1080"
